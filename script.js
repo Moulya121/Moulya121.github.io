5db87f06c01fab2a7bedc585b4c70e7c1a9a81be
Moulya <mona21vishu@gmail.com>
2024-12-22 10:57:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.btn').addEventListener('click', function() {  
+    alert('Redirecting to Resume Page!');  
+});  
+
+$(document).ready(function() {  
+    $('nav ul li a').on('click', function() {  
+        alert('Navigation link clicked!');  
+    });  
+});  
